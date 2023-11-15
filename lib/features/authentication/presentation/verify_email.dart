b9feb7cd340e483fef5ab0e4d6ca442fd58d2a13
Derek Nishimura <nishimuradj@gmail.com>
2023-11-14 14:21:36 -1000

    Created the authentication workflow for logging into the app

---
+++ lib/features/authentication/presentation/verify_email.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:tamagotchi_tamer/features/authentication/presentation/Login.dart';
+import 'package:tamagotchi_tamer/features/home/presentation/Home.dart';
+
+import 'decorations.dart';
+
+
+/// Builds the page to support email verification.
+class VerifyEmailView extends StatelessWidget {
+  const VerifyEmailView({Key? key}) : super(key: key);
+
+  static const routeName = '/verify-email';
+
+  @override
+  Widget build(BuildContext context) {
+    return EmailVerificationScreen(
+      headerBuilder: headerIcon(Icons.verified),
+      sideBuilder: sideIcon(Icons.verified),
+      actions: [
+        EmailVerifiedAction(() {
+          Navigator.pushReplacementNamed(context, HomePage.routeName);
+        }),
+        AuthCancelledAction((context) {
+          FirebaseUIAuth.signOut(context: context);
+          Navigator.pushReplacementNamed(context, LoginPage.routeName);
+        }),
+      ],
+    );
+  }
+}
